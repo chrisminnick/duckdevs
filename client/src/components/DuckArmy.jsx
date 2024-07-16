b6bf1a02decf6565235f255427243e93c778643f
Chris Minnick <chris@minnick.com>
2024-07-16 10:01:50 -0400

    duck army link

--- client/src/components/DuckArmy.jsx
+++ client/src/components/DuckArmy.jsx
@@ -1,5 +1,10 @@
-const DuckArmy = (props) => {
-  return <>Duck Army</>;
+const DuckArmy = () => {
+  return (
+    <>
+      Duck Army hasn&quot;t been converted to React yet.{' '}
+      <a href="/duckdevs-js/duckarmy.html">Visit the vanilla JS version</a>.
+    </>
+  );
 };
 
 export default DuckArmy;
