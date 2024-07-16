d6492b147628d8444b42a286c8d1d96e4e51abd7
Chris Minnick <chris@minnick.com>
2024-07-16 09:52:33 -0400

    faking react leaderboard

--- client/src/components/Leaderboard.jsx
+++ client/src/components/Leaderboard.jsx
@@ -1,7 +1,115 @@
-import React from "react";
-
-const Leaderboard = (props) => {
-  return <></>;
+const Leaderboard = () => {
+  return (
+    <div>
+      <h1>Leaderboard</h1>
+      <table className="leaderboard_table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <td>Wins</td>
+          </tr>
+        </thead>
+        <tbody id="lBody">
+          <tr>
+            <th>Adam Young</th>
+            <td>1</td>
+          </tr>
+          <tr>
+            <th>Aditya Manikadan</th>
+            <td>7</td>
+          </tr>
+          <tr>
+            <th>Angie Chen</th>
+            <td>3</td>
+          </tr>
+          <tr>
+            <th>Anike Braun</th>
+            <td>4</td>
+          </tr>
+          <tr>
+            <th>Arvind Kasiliya</th>
+            <td>2</td>
+          </tr>
+          <tr>
+            <th>Brandon Onyejekwe</th>
+            <td>2</td>
+          </tr>
+          <tr>
+            <th>Chris Minnick</th>
+            <td>3</td>
+          </tr>
+          <tr>
+            <th>Chris Penick</th>
+            <td>3</td>
+          </tr>
+          <tr>
+            <th>Chris Truedson</th>
+            <td>1</td>
+          </tr>
+          <tr>
+            <th>Daniel Savidge</th>
+            <td>3</td>
+          </tr>
+          <tr>
+            <th>Darius Dunn</th>
+            <td>3</td>
+          </tr>
+          <tr>
+            <th>Dea Konjusha</th>
+            <td>3</td>
+          </tr>
+          <tr>
+            <th>Disha Verma</th>
+            <td>4</td>
+          </tr>
+          <tr>
+            <th>Dustin Hagstrom</th>
+            <td>6</td>
+          </tr>
+          <tr>
+            <th>Evan Phillips</th>
+            <td>5</td>
+          </tr>
+          <tr>
+            <th>Paris Machado</th>
+            <td>4</td>
+          </tr>
+          <tr>
+            <th>Sean Molion</th>
+            <td>3</td>
+          </tr>
+          <tr>
+            <th>Shantanu Jain</th>
+            <td>2</td>
+          </tr>
+          <tr>
+            <th>Stephen Antogiovanni</th>
+            <td>1</td>
+          </tr>
+          <tr>
+            <th>Steven Consolini</th>
+            <td>5</td>
+          </tr>
+          <tr>
+            <th>Tucker Raymond</th>
+            <td>4</td>
+          </tr>
+          <tr>
+            <th>Will Bartholomay</th>
+            <td>1</td>
+          </tr>
+          <tr>
+            <th>Will Sibble</th>
+            <td>1</td>
+          </tr>
+          <tr>
+            <th>Zachary Florian</th>
+            <td>4</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Leaderboard;
